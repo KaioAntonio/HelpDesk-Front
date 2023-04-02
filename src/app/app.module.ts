bd2cca1af482df9360098568e04104877b89e888
Kaio Antônio <kaioaar@gmail.com>
2023-04-02 12:41:18 -0300

    add: Componente HOME criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,11 +27,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent, 
-    NavComponent //Sempre tem que declarar
+    NavComponent, HomeComponent //Sempre tem que declarar
   ],
   imports: [
     BrowserModule,
