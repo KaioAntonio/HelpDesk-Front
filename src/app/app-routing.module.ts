4e5c4282ad1ddcd2c3e60e5e6a0cf1e846918c68
Kaio Antônio <kaioaar@gmail.com>
2023-04-02 21:21:26 -0300

    add: Rota para LOGIN

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { NavComponent } from './components/nav/nav.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
+  { path: 'login', component: LoginComponent}
   {
     path: '', component: NavComponent, children: [
       {path: 'home', component: HomeComponent},
