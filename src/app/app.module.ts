f1e131192b377c20f824cfb1fd5a735789547eae
Kaio Antonio <kaioaar@gmail.com>
2023-04-06 17:38:30 -0300

    add: Componente cCHAMADO-LIST criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -40,12 +40,13 @@ import { ClienteCreateComponent } from './components/cliente/cliente-create/clie
 import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
 import { ClienteUpdateComponent } from './components/cliente/cliente-update/cliente-update.component';
 import { ClienteDeleteComponent } from './components/cliente/cliente-delete/cliente-delete.component';
+import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
 
 @NgModule({
   declarations: [
     AppComponent, 
     NavComponent, HomeComponent, HeaderComponent, TecnicoListComponent, LoginComponent, TecnicoCreateComponent, TecnicoUpdateComponent, TecnicoDeleteComponent,
-    ClienteCreateComponent, ClienteListComponent, ClienteUpdateComponent, ClienteDeleteComponent //Sempre tem que declarar
+    ClienteCreateComponent, ClienteListComponent, ClienteUpdateComponent, ClienteDeleteComponent, ChamadoListComponent //Sempre tem que declarar
   ],
   imports: [
     BrowserModule,
